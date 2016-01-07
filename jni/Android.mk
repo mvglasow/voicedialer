6fb6ec6177f23dfea5a9f9c63b361a9d13431bbe
mvglasow <michael -at- vonglasow.com>
2016-01-07 14:52:02 +0100

    Set TARGET_BUILD_TYPE in top-level Android.mk
    Signed-off-by: mvglasow <michael -at- vonglasow.com>

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,6 +4,9 @@
 # Top-level Makefile for ASR
 ###############################################################################
 
+# if we're not building from the AOSP tree, we need to set TARGET_BUILD_TYPE manually
+export TARGET_BUILD_TYPE := debug
+
 # remember root of ASR source tree
 export ASR_ROOT_DIR := $(call my-dir)
 export ASR_MAKE_DIR := $(ASR_ROOT_DIR)/make/asr
