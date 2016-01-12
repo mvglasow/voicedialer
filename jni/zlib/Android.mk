bc94f9c5edf6f519aaed4e97a424aacff7ba6d52
mvglasow <michael -at- vonglasow.com>
2016-01-12 15:21:58 +0100

    Remove shared library target for libz (duplicate module definition)
    Signed-off-by: mvglasow <michael -at- vonglasow.com>

--- jni/zlib/Android.mk
+++ jni/zlib/Android.mk
@@ -29,18 +29,6 @@ LOCAL_SRC_FILES := $(zlib_files)
 ifeq ($(TARGET_ARCH),arm)
   LOCAL_SDK_VERSION := 9
 endif
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_ARM_MODE := arm
-LOCAL_MODULE := libz
-LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS += -O3 -DUSE_MMAP
-LOCAL_SRC_FILES := $(zlib_files)
-ifeq ($(TARGET_ARCH),arm)
-  LOCAL_SDK_VERSION := 9
-endif
 include $(BUILD_STATIC_LIBRARY)
 
 
